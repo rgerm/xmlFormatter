053956801c7ebf8e261420705c399ee899f6a099
ralph <germ@ralphgerm.de>
2012-02-07 16:53:14 +0100

    Added manipulate method for moving tags.

--- src/main/java/org/movsim/xmlformatter/App.java
+++ src/main/java/org/movsim/xmlformatter/App.java
@@ -7,8 +7,10 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.Format;
@@ -24,7 +26,10 @@ public class App {
             builder.setFeature("http://xml.org/sax/features/validation", false);
             builder.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
             builder.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-            final Document document = builder.build(getInputSourceFromFilename(args[0]));
+            Document document = builder.build(getInputSourceFromFilename(args[0]));
+            
+            document = manipulateDocument(document);
+            
             final XMLOutputter outputter = new XMLOutputter();
             Format format = Format.getPrettyFormat();
             format.setIndent("    ");
@@ -40,6 +45,22 @@ public class App {
       
     }
     
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static Document manipulateDocument(Document document) {
+        final Element root = document.getRootElement();
+        Element simulation = root.getChild("SIMULATION");
+        final Element output = simulation.getChild("OUTPUT");
+        Element routes = output.getChild("ROUTES");
+        output.removeChild("ROUTES");
+        List sim = simulation.getChildren();
+        int indexOutput = sim.indexOf(output);
+        if (routes != null) {
+            sim.add(indexOutput, routes.detach());
+            System.out.println("detached routes at: "+indexOutput);
+        }
+        return document;
+    }
+
     public static PrintWriter getWriter(String filename) {
         try {
             final PrintWriter fstr = new PrintWriter(new BufferedWriter(new FileWriter(filename, false)));
